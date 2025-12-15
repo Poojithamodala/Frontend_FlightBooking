7c7eae6a7577665153fc38b441d53202ad04b951
Poojithamodala <poojithamodala26@gmail.com>
2025-12-15 20:10:56 +0530

    updated app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { Register } from './components/register/register';
 import { SearchFlights } from './components/search-flights/search-flights';
 
 export const routes: Routes = [
-    // { path: '', component: Login },
+  // { path: '', component: Login },
   { path: 'register', component: Register },
   { path: 'login', component: Login },
   { path: 'search', component: SearchFlights }
