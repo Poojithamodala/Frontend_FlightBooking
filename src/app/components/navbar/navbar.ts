1e6abcc07de3c33cdae264b6561c34bd9602062c
Poojithamodala <poojithamodala26@gmail.com>
2025-12-19 09:50:12 +0530

    Updated

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth';
 import { CommonModule } from '@angular/common';
 
@@ -10,15 +10,29 @@ import { CommonModule } from '@angular/common';
   styleUrl: './navbar.css',
 })
 export class Navbar {
-  constructor(private authService: AuthService, private router: Router) { }
+  // isHome=false;
+  constructor(private authService: AuthService, private router: Router, private cdr: ChangeDetectorRef) { 
+    // this.router.events.subscribe(event => {
+    //   if (event instanceof NavigationEnd) {
+    //     this.isHome = this.router.url === '/' || this.router.url === '';
+    //     this.cdr.detectChanges();
+    //   }
+    // });
+  }
 
   isLoggedIn() {
     return this.authService.isLoggedIn();
   }
 
-  logout() {
-    this.authService.logout().subscribe(() => {
-      this.router.navigate(['/']);
-    });
-  }
+logout() {
+  this.authService.logout().subscribe({
+    next: () => {
+      alert('Logged out successfully!');
+      this.router.navigate(['/login']); // redirect to login page
+    },
+    error: () => {
+      alert('Error logging out, please try again.');
+    }
+  });
+}
 }
