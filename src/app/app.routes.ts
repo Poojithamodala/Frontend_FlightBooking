4221b5508bd42e61af47f1a38c772e4535ff6d4b
Poojithamodala <poojithamodala26@gmail.com>
2025-12-19 09:49:15 +0530

    Updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Register } from './components/register/register';
 import { SearchFlights } from './components/search-flights/search-flights';
+import { App } from './app';
+import { Profile } from './components/profile/profile';
+import { BookFlight } from './components/book-flight/book-flight';
 
 export const routes: Routes = [
-  // { path: '', component: Login },
+  { path: '', component: App },
   { path: 'register', component: Register },
   { path: 'login', component: Login },
-  { path: 'search', component: SearchFlights }
+  {path: 'profile', component: Profile},
+  { path: 'search', component: SearchFlights },
+  { path: 'book', component: BookFlight }
 ];
