f0f43c0d468560eda92dae396a5f43047a54628c
Poojithamodala <poojithamodala26@gmail.com>
2025-12-19 09:55:38 +0530

    Added profile

---
+++ src/app/components/profile/profile.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { AuthService } from '../../services/auth';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,   
+  imports: [CommonModule],
+  templateUrl: './profile.html',
+  styleUrl: './profile.css',
+})
+export class Profile {
+  user!: User;
+  loading = true;
+  error = '';
+
+  constructor(private authService: AuthService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.authService.getProfile().subscribe({
+      next: (data) => {
+        this.user = data;
+        this.loading = false;
+        this.cdr.detectChanges();
+      },
+      error: () => {
+        this.error = 'Failed to load profile';
+        this.loading = false;
+        this.cdr.detectChanges();
+      }
+    });
+  }
+}
